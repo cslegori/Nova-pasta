25d4e0f32d4cdbbede9a6ad46aa27d47eac042da
cslegori <cida.legori@gmail.com>
2025-11-04 21:11:19 -0300

    aula05

--- aula05/duracaoST.py
+++ aula05/duracaoST.py
@@ -4,7 +4,7 @@ TITULO = "Calculadora de Duração de Tempo"
 st.set_page_config(page_title=TITULO)
 st.title(TITULO) 
 #Entrada de dados
-tempo = st.number_input("Digite o tempo em segundos:")
+tempo = st.number_input("Digite o tempo em segundos:", min_value=0, step=1, help="Insira a duração total em segundos para converter em horas, minutos e segundos")
 #Processamento dos dados
 horas = tempo // 3600 #Calculo das horas
 minutos = (tempo % 3600) // 60 #Calculo dos minutos
