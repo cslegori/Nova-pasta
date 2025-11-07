91331e84f52dde41dc2dc5b70a1fc9fbc9d9a76b
cslegori <cida.legori@gmail.com>
2025-11-06 21:46:44 -0300

    temperatura

--- aula07/dardoST.py
+++ aula07/dardoST.py
@@ -1,4 +1,9 @@
 import streamlit as st
+def grafico(datsu1, datsu2, datsu3):
+    #Apresentação de grafico exibindo lançamento
+    st.area_chart([1,2,3,4,5,6,datsu1], use_container_width=True,height=200,color="#eaff00")
+    st.area_chart([1,2,3,4,5,6,datsu2], use_container_width=True,height=200,color="#f65200")
+    st.area_chart([1,2,3,4,5,6,datsu3], use_container_width=True,height=200,color="#5100ff")
 st.title("🎯Simulação de lançamento de Dardos🎯")
 '''Simulação de lançamento de tres dardos.
 O objetivo do aplicativo é mostrar o dardo com a maior distancia'''
@@ -11,13 +16,19 @@ with coluna2:
     dardo2 = st.number_input("Distancia 2° Dardo",min_value=0)
 with coluna3:
     dardo3 = st.number_input("Distancia 3° Dardo",min_value=0)
+maior_distancia = max(dardo1,dardo2,dardo3)
 #Estrutura de controle de decisao
 if (dardo1>dardo2) and (dardo1>dardo3):
     dardo_vencedor = "Dardo 1"
-elif(dardo2>dardo1) and (dardo2>dardo3):
+elif(dardo2 > dardo1) and (dardo2 > dardo3):
     dardo_vencedor = "Dardo 2"
-else:
+elif (dardo3 > dardo1) and (dardo3 > dardo2):
     dardo_vencedor = "Dardo 3"
+elif (dardo1 == dardo2) or(dardo1 == dardo3) or (dardo2 == dardo3):
+    dardo_vencedor = "Empate"
 #Saida de dados
 if st.button("Apresentar resultados de lançamento"):
-    st.write(f"O dardo com a maior distancia é o {dardo_vencedor}")
+    if dardo_vencedor == "Empate":
+        st.write ("Houve empate sem vencedor")
+    st.write(f"O dardo com a maior distancia é o {dardo_vencedor} com {maior_distancia}")
+    grafico(dardo1,dardo2,dardo3)
---
+++ aula07/temperaturaST.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+def Celsius_Fahrenheit(t):
+    return (t * 1.8) + 32
+def Celsius_Kelvin(t):
+    return temp + 273.15
+def F_Celsius(t):
+    return (t - 32) * 5/9
+def F_Kelvins(t):
+    return F_Celsius(t) + 273.15
+def K_Celsius(t):
+      return t - 273.15
+def F_Fahrenheit(t):
+        return Celsius_Fahrenheit(K_Celsius(t)) 
+#Problema temperatura
+st.sidebar.title("Conversor de Temperatura")
+st.title("Conversor de Temperatura")
+st.sidebar.markdown("Converte a temperatura entre Celsius, Fahrenheit e Kelvin")
+celsius_selecionado = st.sidebar.checkbox("Celsius",key="temp_celsius")
+fahrenheit_selecionado = st.sidebar.checkbox("Fahrenheit",key="temp_fahrenheit")
+kelvin_selecionado = st.sidebar.checkbox("Kelvin",key="temp_Kelvin")
+opcao_selecionada = st.sidebar.radio(options=['Celsius', 'Kelvin', 'Fahrenheit'],key="opcao_radio",label="Selecionar")
+#Entrada de dados
+temp = st.number_input("Valor da temperatura",format="%.2f",step=1.0)
+# if celsius_selecionado + fahrenheit_selecionado + kelvin_selecionado > 1:
+#         st.sidebar.error("Por favor, selecione apenas uma unidade de tem")
+#Processamento de dados
+if st.button("Converter",icon= "🌡"):
+            if celsius_selecionado:
+                    st.write(f"{temp}°C em Fahrenheit:{Celsius_Fahrenheit(temp):.2}°F")
+                    st.write(f"{temp}°C em Kelvin: {Celsius_Kelvin(temp):.2f}K")
+            elif fahrenheit_selecionado:
+                   st.write(f"{temp}°F em Clesius:{F_Celsius(temp):.2f}°C")
+                   st.write(f"{temp}°F em Kelvin:{F_Kelvin(temp):.2f}K")
+            elif kelvin_selecionado:
+                   st.write(f"{temp}k em Clesius:{K_Celsius(temp):.2F} °C")
+                   st.write(f"{temp}k em Fahrenheit{K_Fahrenheit(temp):.2f} °F")
