1e5cd79a50a2d0e5c6f8ba71d46da7ce2cfb7619
cslegori <cida.legori@gmail.com>
2025-11-07 20:30:04 -0300

    atividade02

--- aula08/atividade02ST.py
+++ aula08/atividade02ST.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import math as mt
-#Problema Triangulo
 st.title("Verificação de Triângulo e Cálculo de Perímetro ou Área")
-valor_a = st.number_input("Valor de A:", min_value=0.0)
-valor_b = st.number_input("Valor de B:", min_value=0.0)
-valor_c = st.number_input("Valor de C:", min_value=0.0)
+valor_a = st.number_input("Lado A:", min_value=0.0)
+valor_b = st.number_input("Lado B:", min_value=0.0)
+valor_c = st.number_input("Lado C:", min_value=0.0)
 if st.button("Verificar Triângulo"):
     if (valor_a < valor_b + valor_c) and (valor_b < valor_a + valor_c) and (valor_c < valor_a + valor_b):
         st.write("Os valores formam um triângulo.")
@@ -14,4 +13,4 @@ if st.button("Verificar Triângulo"):
         st.write("Os valores não formam um triângulo.")
         area = (valor_b * valor_c) / 2
         st.write(f"A área do triângulo é: {area:.2f}")
-        
+          
