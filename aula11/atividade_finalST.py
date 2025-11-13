2943c41e15bf266fc95e3f9f1ad3e9e5f78691c2
cslegori <cida.legori@gmail.com>
2025-11-13 18:39:16 -0300

    atividade_final

--- aula11/atividade_finalST.py
+++ aula11/atividade_finalST.py
@@ -5,5 +5,5 @@ nome = st.text_input("Nome")
 idade = st.number_input("Digite a idade",min_value=0, step=1)
 altura = st.number_input("Digite a altutra", max_value=2,5)
 MAXIMO_PESSOAS = 5
-
+MEDIA = ()
 
