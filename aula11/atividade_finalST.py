823805ede499d77f117cb8ddb3468098e1aa025d
cslegori <cida.legori@gmail.com>
2025-11-12 21:44:15 -0300

    atividade

---
+++ aula11/atividade_finalST.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.title("Atividade Final")
+nome = st.text_input("Nome")
+idade = st.number_input("Digite a idade",min_value=0, step=1)
+altura = st.number_input("Digite a altutra", max_value=2,5)
+MAXIMO_PESSOAS = 5
+
+
